7c9a7f0d7da7da9fb1c692b08d2481bab39a49f5
Atticus Helvig <atticushelvig@gmail.com>
2025-06-08 13:20:46 -0700

    feat: adjust neoscroll timings

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -4,17 +4,17 @@ neoscroll.setup({
 })
 
 local keymap = {
-	["K"] = function() neoscroll.ctrl_u({ duration = 100 }) end,
-	["J"] = function() neoscroll.ctrl_d({ duration = 100 }) end,
-	["<C-u>"] = function() neoscroll.ctrl_u({ duration = 100 }) end,
-	["<C-d>"] = function() neoscroll.ctrl_d({ duration = 100 }) end,
-	["<C-b>"] = function() neoscroll.ctrl_b({ duration = 150 }) end,
-	["<C-f>"] = function() neoscroll.ctrl_f({ duration = 150 }) end,
+	["K"] = function() neoscroll.ctrl_u({ duration = 75 }) end,
+	["J"] = function() neoscroll.ctrl_d({ duration = 75 }) end,
+	["<C-u>"] = function() neoscroll.ctrl_u({ duration = 75 }) end,
+	["<C-d>"] = function() neoscroll.ctrl_d({ duration = 75 }) end,
+	["<C-b>"] = function() neoscroll.ctrl_b({ duration = 125 }) end,
+	["<C-f>"] = function() neoscroll.ctrl_f({ duration = 125 }) end,
 	["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 25 }) end,
 	["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 25 }) end,
-	["zt"] = function() neoscroll.zt({ half_win_duration = 100 }) end,
-	["zz"] = function() neoscroll.zz({ half_win_duration = 100 }) end,
-	["zb"] = function() neoscroll.zb({ half_win_duration = 100 }) end,
+	["zt"] = function() neoscroll.zt({ half_win_duration = 75 }) end,
+	["zz"] = function() neoscroll.zz({ half_win_duration = 75 }) end,
+	["zb"] = function() neoscroll.zb({ half_win_duration = 75 }) end,
 }
 
 local modes = { 'n', 'v', 'x' }
