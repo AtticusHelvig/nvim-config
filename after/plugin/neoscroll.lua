3cce4b262b8e94185fcac197d0480ab67301e36c
Atticus Helvig <atticushelvig@gmail.com>
2025-06-08 13:35:16 -0700

    fix: conflicting keybind

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -11,7 +11,6 @@ local keymap = {
 	["<C-b>"] = function() neoscroll.ctrl_b({ duration = 125 }) end,
 	["<C-f>"] = function() neoscroll.ctrl_f({ duration = 125 }) end,
 	["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 25 }) end,
-	["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 25 }) end,
 	["zt"] = function() neoscroll.zt({ half_win_duration = 75 }) end,
 	["zz"] = function() neoscroll.zz({ half_win_duration = 75 }) end,
 	["zb"] = function() neoscroll.zb({ half_win_duration = 75 }) end,
