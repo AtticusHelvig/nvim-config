f6000ab5433aab8f382f25831ae613e914b839d8
Atticus Helvig <atticushelvig@gmail.com>
2025-06-10 10:18:02 -0700

    feat: unbind arrow keys

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,3 +7,13 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Arrow keys
+vim.keymap.set('n', '<S-Up>', "<nop>")
+vim.keymap.set('n', '<S-Down>', "<nop>")
+vim.keymap.set('n', '<S-Left>', "<nop>")
+vim.keymap.set('n', '<S-Right>', "<nop>")
+vim.keymap.set('n', '<Up>', "<nop>")
+vim.keymap.set('n', '<Down>', "<nop>")
+vim.keymap.set('n', '<Left>', "<nop>")
+vim.keymap.set('n', '<Right>', "<nop>")
